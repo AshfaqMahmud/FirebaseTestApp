a331d12aabc91c8f9368ee3f924826e48875cfe0
AshfaqMahmud <ash.mse21@gmail.com>
2022-11-12 01:13:05 +0600

    Initial Commit

---
+++ FirebaseTestApp/FirebaseTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestAppApp.swift
+//  FirebaseTestApp
+//
+//  Created by Ashfaq on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
